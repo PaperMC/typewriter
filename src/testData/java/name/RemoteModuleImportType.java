0b5d7a047258a9db3c087a45b2d5f3a557dd58a7
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2024-12-09 19:27:05 +0100

    resolves transitive modules

--- src/testData/java/name/RemoteModuleImportType.java
+++ src/testData/java/name/RemoteModuleImportType.java
@@ -1,11 +1,13 @@
 package name;
 
 import module java.compiler;
+import module java.sql;
 
 public class RemoteModuleImportType {
     {
         var a = SourceVersion.RELEASE_23;
         var b = AnnotationMirror.class;
-        var c = java.util.List.class;
+        var c = XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI;
+        var d = java.util.List.class;
     }
 }
